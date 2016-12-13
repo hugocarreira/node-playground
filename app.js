f10676e26b4d0076026fbbd9c9ddbecfc537ce84
Hugo Carreira <hugo.colombo23@gmail.com>
2016-12-12 16:21:26 -0800

    hello world expressJs

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('Olá Express!');
+});
+
+app.listen(3000, function() {
+  console.log('Rodando em http://localhost:3000 com Express');
+});
